84e8689d6dea4e5a0affe8b1ba3675186f12d835
Farah Yasser <farahabotaleb25@gmail.com>
2025-12-31 22:30:50 +0200

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import { useState } from "react";
 import { User } from "lucide-react";
 
